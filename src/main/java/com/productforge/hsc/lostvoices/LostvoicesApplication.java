557bec976bec507aba3c01894115ae35f2dada3a
bhanu <bhanu.yandra@gmail.com>
2019-06-02 09:46:59 +0100

    Initial commit

---
+++ src/main/java/com/productforge/hsc/lostvoices/LostvoicesApplication.java
@@ -0,0 +1,13 @@
+package com.productforge.hsc.lostvoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostvoicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostvoicesApplication.class, args);
+	}
+
+}
